64775aa290dc69dad734fb3c79da0db25a709a74
David Rogers <davidorog@mac.com>
2017-12-29 12:56:32 -0500

    Styled action card

--- commutestream-nativeads/src/main/java/com/commutestream/nativeads/SecondaryPopUp.java
+++ commutestream-nativeads/src/main/java/com/commutestream/nativeads/SecondaryPopUp.java
@@ -4,8 +4,11 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -25,12 +28,16 @@ public class SecondaryPopUp {
     private TextView subtitleView;
     private TextView headlineView;
     private TextView bodyView;
+    private TextView advertiserView;
     private FrameLayout heroFrame;
     private ImageView heroImageView;
     private WebView heroWebView;
     private LinearLayout actionsLayout;
     private Button action1Button;
     private ImageButton closeButton;
+    private ViewTreeObserver heroFrameTreeObserver;
+    private Boolean heroHeightAdjusted;
+
 
 
     public SecondaryPopUp(Activity activity) {
@@ -43,6 +50,7 @@ public class SecondaryPopUp {
         logoView = view.findViewById(R.id.secondary_ad_logo);
         headlineView = view.findViewById(R.id.secondary_ad_headline);
         bodyView = view.findViewById(R.id.secondary_ad_body);
+        advertiserView = view.findViewById(R.id.secondary_ad_advertiser);
         heroFrame = view.findViewById(R.id.secondary_ad_hero);
         actionsLayout = view.findViewById(R.id.secondary_ad_actions);
         closeButton = view.findViewById(R.id.secondary_ad_close);
@@ -52,7 +60,33 @@ public class SecondaryPopUp {
                 popup.dismiss();
             }
         });
+
+        heroHeightAdjusted = false;
+        heroFrameTreeObserver = heroFrame.getViewTreeObserver();
+        heroFrameTreeObserver.addOnGlobalLayoutListener( new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+//                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+//                    this.heroFrameTreeObserver.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+//                } else {
+//                    this.heroFrameTreeObserver.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+//                }
+                int width  = heroFrame.getMeasuredWidth();
+                int height = heroFrame.getMeasuredHeight();
+
+                int newHeight = (int)Math.floor(width * 0.5225);
+                if(!heroHeightAdjusted){
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width,newHeight);
+                    heroFrame.setLayoutParams(params);
+                    heroHeightAdjusted = true;
+                }
+
+
+            }
+        });
     }
+
+
 
     public void displayAd(Ad ad) {
         titleView.setText(ad.getSecondaryAction().getTitle());
@@ -60,19 +94,24 @@ public class SecondaryPopUp {
         logoView.setImageBitmap(ad.getLogo().getLogo());
         headlineView.setText(ad.getHeadline().getHeadline());
         bodyView.setText(ad.getBody().getBody());
+        advertiserView.setText(ad.getAdvertiser().getAdvertiser());
 
         //TODO add image or web view depending on hero kind
         heroImageView = new ImageView(activity);
         heroImageView.setImageBitmap(ad.getHero().getImage());
         heroFrame.removeAllViews();
         heroFrame.addView(heroImageView, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+
+
         //TODO add action buttons
         actionsLayout.removeAllViews();
         for (final ActionComponent action : ad.actions) {
             Button actionButton = new Button(activity);
+
             actionButton.setText(action.getTitle());
             actionButton.setBackgroundColor(action.getColors().getBackground());
             actionButton.setTextColor(action.getColors().getForeground());
+
             actionButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -82,10 +121,15 @@ public class SecondaryPopUp {
                     activity.startActivity(i);
                 }
             });
-            actionsLayout.addView(actionButton);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0,LinearLayout.LayoutParams.MATCH_PARENT);
+            params.weight = 1.0f;
+            params.setMargins(5,0,5,0);
+            actionsLayout.addView(actionButton, params);
         }
 
         popup.showAtLocation(activity.findViewById(android.R.id.content), Gravity.CENTER, 0, 0);
         popup.update();
+
     }
 }
