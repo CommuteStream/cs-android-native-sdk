a2c2ec69a710b76d0adb3efefa685062d0a2bb02
Tom Burdick <thomas.burdick@gmail.com>
2018-01-23 15:42:59 -0600

    release: 1.2.9

--- commutestream-nativeads/src/main/java/com/commutestream/nativeads/AdsController.java
+++ commutestream-nativeads/src/main/java/com/commutestream/nativeads/AdsController.java
@@ -38,7 +38,7 @@ public class AdsController {
         void onAds(List<Ad> ads);
     }
 
-    public final static String SDK_VERSION = "1.2.8";
+    public final static String SDK_VERSION = "1.2.9";
     private UUID adUnit;
     private UUID aaid = UUID.fromString("00000000-0000-0000-0000-000000000000");
     private boolean limitTracking = true;
